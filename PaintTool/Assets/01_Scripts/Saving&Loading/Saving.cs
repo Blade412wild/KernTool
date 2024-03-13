c002cf93a21fdfd6deaa0d21950ea8d2964e366f
Blade412wild <nathanhoh951@gmail.com>
2024-03-13 20:40:07 +0100

    Saving & loading test works. now for texture

--- PaintTool/Assets/01_Scripts/Saving&Loading/Saving.cs
+++ PaintTool/Assets/01_Scripts/Saving&Loading/Saving.cs
@@ -8,20 +8,39 @@ using static UnityEditor.Progress;
 public class Saving : MonoBehaviour
 {
 
-    [SerializeField] private string fileName;
     [SerializeField] private string name;
-    [SerializeField] private int leeftijd;
+    [SerializeField] private int age;
     [SerializeField] private float height;
 
+    private data dataTest;
+
     private string fullPath;
+
+    private void Start()
+    {
+        dataTest = new data
+        {
+            _name = name,
+            _age = age,
+            _height = height
+
+        };
+    }
+
+    public struct data
+    {
+        public string _name;
+        public int _age;
+        public float _height;
+    }
 
     public void OnLoadButton()
     {
         Debug.Log("Attempting Loading");
         string[] options = StandaloneFileBrowser.OpenFilePanel("Open File", Application.persistentDataPath, "txt", false);
         if (options.Length == 0) return;
-
-        string url = options[0];
+        string filepath = options[0];
+        Load(filepath);
     }
 
     public void OnSaveButton()
@@ -29,20 +48,26 @@ public class Saving : MonoBehaviour
         Debug.Log("Attempting Saving");
         string filePath = StandaloneFileBrowser.SaveFilePanel("Save File", Application.persistentDataPath, "Wow_Je_Saved_Een_Keer.txt", "txt");
         if (filePath.Length == 0) return;
-        Save(filePath);
+        Save(filePath, dataTest);
     }
 
-    private void Save(string _filePath)
+    private void Save(string _filePath, data dataPackage)
     {
         StreamWriter writer = new StreamWriter(_filePath);
-        writer.WriteLine(JsonUtility.ToJson(this, true));
+        writer.WriteLine(JsonUtility.ToJson(dataPackage, true));
         writer.Close();
         writer.Dispose();
         Debug.Log("Saved File To : " + _filePath);
     }
 
-    private void Load()
+    private void Load(string _filePath)
     {
+        StreamReader reader = new StreamReader(_filePath);
+        data dataSetting = JsonUtility.FromJson<data>(reader.ReadToEnd());
+        name = dataSetting._name;
+        age = dataSetting._age;
+        height = dataSetting._height;
+        Debug.Log("Saved File To : " + _filePath);
 
     }
 
